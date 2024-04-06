f72ffcba5ecb390edae634852ceaf45f92fea2e0
ChrisManagable <chrismg@my.yorku.ca>
2024-04-06 11:11:01 -0400

    Updated Home Activity to send name

--- Tamagotchi_Testing/app/src/main/java/com/example/waveletmenu/HomeActivity.java
+++ Tamagotchi_Testing/app/src/main/java/com/example/waveletmenu/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.waveletmenu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
@@ -10,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private ImageButton b1, b2, b3;
-
+    private EditText name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +19,24 @@ public class HomeActivity extends AppCompatActivity {
         b1 = findViewById(R.id.wavelet_button);
         b2 = findViewById(R.id.drag_drop_button);
         b3 = findViewById(R.id.linear_button);
+        name = findViewById(R.id.namefield);
     }
 
     public void click(View v){
+        if(name.getText().toString() == "Tap To Enter Name")
+            name.setText("Subject");
         if(v == b1){
-            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            intent.putExtra("Name", name.getText().toString());
+            startActivity(intent);
         }else if(v == b2){
-            startActivity(new Intent(HomeActivity.this, DragDropActivity.class));
+            Intent intent = new Intent(HomeActivity.this, DragDropActivity.class);
+            intent.putExtra("Name", name.getText().toString());
+            startActivity(intent);
         }else if(v == b3){
-            startActivity(new Intent(HomeActivity.this, LinearActivity.class));
+            Intent intent = new Intent(HomeActivity.this, LinearActivity.class);
+            intent.putExtra("Name", name.getText().toString());
+            startActivity(intent);
         }
     }
 }
